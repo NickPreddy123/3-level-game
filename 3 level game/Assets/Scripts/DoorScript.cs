1bfdac586ccb1e2a104c2d3a45a8e9cbbc34871c
Nick Botelho <n.preddy@surreyschools.ca>
2022-11-23 14:50:11 -0800

    Seventh Commit
    I have made a code for a working door and am still continuing on my donkey kong type level it took me all class to make the door script

--- 3 level game/Assets/Scripts/DoorScript.cs
+++ 3 level game/Assets/Scripts/DoorScript.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    public GameObject Door;
+    public Transform DoorLeft;
+    public Transform DoorRight;
+    public float OpenAmount;
+    public float CloseAmount;
     
-   
+    void MoveObject() //If you are facing X+ use positive numbers for Open And close amount if X- use negative numbers
+    {
+        DoorLeft.transform.position = DoorLeft.transform.position + new Vector3(OpenAmount * Time.deltaTime, 0f, 0f);
+
+        DoorRight.transform.position = DoorRight.transform.position + new Vector3(OpenAmount * -Time.deltaTime, 0f, 0f);
+    }
+
+    void StopObject()
+    {
+        
+        DoorLeft.transform.position = DoorLeft.transform.position + new Vector3(CloseAmount * -Time.deltaTime, 0f, 0f);
+
+        DoorRight.transform.position = DoorRight.transform.position + new Vector3(CloseAmount * Time.deltaTime, 0f, 0f);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Door.GetComponent<Animation>().Play("Left Door Open");
+            MoveObject();
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Door.GetComponent<Animation>().Play("Left Door Close");
+            StopObject();
         }
     }
 }
